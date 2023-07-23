ed7c2d9318cf1a4d2373815bf6e7072760c5baa3
huan-zz3 <2805033624@qq.com>
2023-07-23 14:35:37 +0800

    Initial commit

---
+++ espSoftwareSerial.h
@@ -0,0 +1,32 @@
+#ifndef espSoftwareSerial_H
+#define espSoftwareSerial_H
+
+#include <Arduino.h>
+
+
+
+namespace eSSNamespace{
+	
+	
+	class eSSModel{
+		public:
+			
+		private:
+			TaskHandle_t xHandle;
+			
+			
+	};
+	//eSSModel eSS1, eSS2;
+	
+	
+	
+}
+
+namespace eSSEnv{
+	/*using eSSNamespace::eSS1;
+	using eSSNamespace::eSS2;*/
+	
+	
+}
+
+#endif
